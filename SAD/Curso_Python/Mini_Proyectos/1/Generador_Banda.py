0123f6df4154a54742e8e67e1accb48598561ea0
GMARTOSMC <gmartos96@tutamail.com>
2024-11-30 15:49:57 +0100

    Mini Proyecto 1

---
+++ SAD/Curso_Python/Mini_Proyectos/1/Generador_Banda.py
@@ -0,0 +1,11 @@
+print("Bienvenido al generador de nombres de bandas musicales\n")
+
+ciudad = input("Cual es la ciudad en la que naciste?\n")
+
+mascota = input("¿Cual es el nombre de tu mascota?\n")
+
+
+print("La banda se llamará " + mascota + " de " + ciudad)
+
+
+
