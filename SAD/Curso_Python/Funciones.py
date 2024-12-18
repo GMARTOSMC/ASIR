ae7bc4f56b69a648f4c805aff0be4cbd12da4a7a
Gonzalo Martos <gmartos96@tutamail.com>
2024-12-18 14:53:58 +0100

    calculadroa

---
+++ SAD/Curso_Python/Funciones.py
@@ -0,0 +1,33 @@
+def my_function():
+    print("Hola Mundo")
+
+my_function()
+
+def suma(var1:int, var2:int):
+    print(var1+var2)
+
+suma(2,5)
+
+pi = 3.14
+
+def multiplicar_por_pi(valor:int):
+    print(valor * pi)
+
+suma(2,5)
+suma(4,3)
+suma(2,9)
+
+def multiplicar_por_un_valor_dado(value):
+    print(value * 34)
+
+multiplicar_por_un_valor_dado(10)
+
+def resta(var1, var2):
+    resultado = var1 - var2
+    return resultado
+
+var3 = resta(10,2)
+
+print(var3)
+print(resta(10,2))
+
